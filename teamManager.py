314ae9d647b4a6ef8ff70e293fb429b615fc1fc5
KahlilMonteiro-lpsr <rc.monteiro.kahlil@leadps.org>
2016-01-27 15:10:24 -0800

    Update teamManager.py

--- teamManager.py
+++ teamManager.py
@@ -1,24 +1,40 @@
- self.age = age
-    self.goals = goals
-  def printStats(self):
+ # makes a class function with an object
+ class Player(object):
+  def __init__(self, name, age, goals):
+   self.name = name
+   self.age = age
+   self.goals = goals
+   # makes a def function to print the players stats
+   def printStats(self):
     print("name: " + (self.name))
     print("age: " + str(self.age))
     print("goals: " + str(self.goals))
-    
+# makes a print statement to print out the functions to add players
 print("Welcome to fifa training camp, can you help us get some new players")
 print("(1) add a player")
 print("(2) print all players")
 print("(3) exit fifa")
+# makes a list function
 playGame = []
+# makes player_choice into an integer
 player_choice = int(input())
+# makes a while statement that makes player_choice stop the code after the user finishes making players
 while player_choice != (3):
+# makes a if statement so if the user wants to enter a player, they enter the number one
   if player_choice == 1:
+# asks the user the question that asks the user for the players name 
     print("What is the players name?")
+# makes playName into a string
     playName = input()
+# asks the user the question that asks the user for the players age
     print("what is the players age?")
+# makes playAge in an integer
     playAge = int(input())
+# asks the user the question that asks the user how many goals the player has made
     print("How many goals did the player make?")
+# makes playGoals into an integer
     playGoals = int(input())
+    
     playGame.append(Player(playName, playAge, playGoals))
     print("What do you want to do now?")
     player_choice = int(input())
