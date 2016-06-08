c3e33a62b33739a86915ab8aaed9c8e220f6f701
LPS Student <lps@lps-1011PX.(none)>
2016-06-08 15:04:53 -0700

    My Cipher coding

--- 6-3CaesarsCipher/applyCipher.py
+++ 6-3CaesarsCipher/applyCipher.py
@@ -3,41 +3,51 @@
 # using Caesars Cipher
 #
 # Author: rc.monteiro.kahlil [at] leadps.org
-
+import string
 #  makes a mapping of alphabet to decoded alphabet
 # arguments: key
 # returns: dictionary of mapped letters
-def createDictionary():
-
-	# placeholder
-	return {}
+def createDictionary(key):
+	alphabet = list(string.ascii_lowercase)
+	alphabet1 = {}
+	num = 0
+	for n in alphabet:
+		alphabet1[n] = (alphabet[(key + num) % 26])
+		num += 1
+	return alphabet1
 
 # receive: user's encrypted message
 # arguments: none
 # return: encoded messge
-def getMessage():
-	pass
+def getMessage(message):
+	return message
 
 # for each letter in the message, decodes and record
 # arguments:encoded message, dictionary
 # returns decoded message
 def decodedMessage(message, dictionary):
-	pass
+	code = ''
+	for m in message:
+		newCode = dictionary[m]
+		code = code + newCode
+	return code
 
 # outputs the message to the terminal 
 # arguments: decoded message
 # returns: none
-def printMessage(message):
-	pass
+def printMessage(decodemessage):
+	print(decodemessage)
 
 # execution starts here
 
 # ask user for key
-print("What key would you like to use decode?")
-
+print("What key would you like to use decode?(1-25)")
 key = int(raw_input())
+print("What message would you like to decode?")
+message = raw_input()
+print("Here's the decoding of your message:")
 
-dictionary = createDictionart(key)
-encodedMessage = getMessage()
-decodedMessage = decodedMessage(encodedMessage, dictionary)
-printMessage(decodedMessage)
+dictionary = createDictionary(key)
+encodeMessage = getMessage(message)
+decodeMessage = decodedMessage(encodeMessage, dictionary)
+printMessage(decodeMessage)
